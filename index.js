c7b20084bedcd2f43f0db6f5285644af0f4a6893
Gustaf Buchanan <gnhbuchanan@gmail.com>
2019-05-17 22:05:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,16 @@ function takeANumber (line, name){
   
 }
 
+
+
+function nowServing(line){
+  
+  if(line.length === 0){
+    return 'There is nobody waiting to be served!'
+  }
+  
+  let element = line.unshift();
+  
+  return element; 
+  
+}
